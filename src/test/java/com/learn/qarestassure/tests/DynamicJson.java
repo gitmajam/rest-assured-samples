9769e228b59fd600b2e9eee399f2bb30e2e9604d
marlon <marlon@station2.station2>
2022-10-16 23:50:14 -0500

    updated utilities class with jackson parser

--- src/test/java/com/learn/qarestassure/tests/DynamicJson.java
+++ src/test/java/com/learn/qarestassure/tests/DynamicJson.java
@@ -6,9 +6,6 @@ import java.util.Map;
 
 import org.testng.annotations.Test;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.learn.qarestassured.testframework.utilities.DataProviders;
 import com.learn.qarestassured.testframework.utilities.Utilities;
@@ -20,28 +17,36 @@ public class DynamicJson {
 	public static String dataProviderFilePath = "src/test/resources/providerFiles/dynamic-json-data.csv";
 
 	@Test(dataProvider = "csvReaderMethod", dataProviderClass = DataProviders.class, groups = { "smoke" })
-	public void addBook(Map<String, String> provider) throws JsonMappingException, JsonProcessingException {
+	public void addBook(Map<String, String> provider){
 		
-		// Creating an instance of ObjectMapper class
-		ObjectMapper objectMapper = new ObjectMapper();
-		
-		// Get ObjectNode representation of json as json is an Object
-		ObjectNode objectNode = objectMapper.readValue(provider.get("body"), ObjectNode.class);
-		objectNode.put("name", provider.get("name"));
-		objectNode.put("isbn", provider.get("isbn"));
-		objectNode.put("aisle", provider.get("aisle"));
-//		ObjectNode arrayNode = (ObjectNode) objectNode.withArray("nested").get(2);
-//		arrayNode.put("color", provider.get("color"));
+		ObjectNode bodyAddBookNode = Utilities.rawToJsonObject(provider.get("bodyAddBook"));
+		bodyAddBookNode.put("name", provider.get("name"));
+		bodyAddBookNode.put("isbn", provider.get("isbn"));
+		bodyAddBookNode.put("aisle", provider.get("aisle"));
 
 		RestAssured.baseURI = provider.get("baseUri");
+		
 		String response = given().log().all().header("Content-Type", provider.get("contentType"))
-			.body(objectNode)
-			.when().log().all().post(provider.get("resource")).then().assertThat().log().all()
+			.body(bodyAddBookNode)
+			.when().post(provider.get("resourceAddBook")).then().log().all().assertThat()
 			.statusCode(Integer.parseInt(provider.get("statusCode")))
 			.extract().response().asString();
 		
-		String msg = Utilities.rawToJson(response).get("Msg") == null ? Utilities.rawToJson(response).get("msg"): Utilities.rawToJson(response).get("Msg");
-		System.out.println(msg);
+		// get the book ID from the response
+		
+		String bookID = Utilities.rawToJsonObject(response).get("ID").textValue();
+		
+		// edit the delete body with the current ID
+		
+		ObjectNode bodyDeleteBookNode = Utilities.rawToJsonObject(provider.get("bodyDeleteBook"));
+		bodyDeleteBookNode.put("ID", bookID);
+		
+		// delete the book we just created
+		
+		given().log().all().header("Content-Type", provider.get("contentType"))
+		.body(bodyDeleteBookNode)
+		.when().delete(provider.get("resourceDeleteBook"))
+		.then().log().all().assertThat().statusCode(200);
 	}
 
 }
--- src/test/java/com/learn/qarestassured/testframework/utilities/Utilities.java
+++ src/test/java/com/learn/qarestassured/testframework/utilities/Utilities.java
@@ -1,5 +1,10 @@
 package com.learn.qarestassured.testframework.utilities;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import io.restassured.path.json.JsonPath;
 
 public class Utilities {
@@ -8,4 +13,26 @@ public class Utilities {
 			JsonPath js1 = new JsonPath(response);
 			return js1;
 		}
+		
+		public static ObjectNode rawToJsonObject(String response ) {
+			
+			// Creating an instance of ObjectMapper class
+			ObjectMapper objectMapper = new ObjectMapper();
+			
+			// Get ObjectNode representation of json as json is an Object
+			ObjectNode objectNode = null;
+			
+			try {
+				objectNode = objectMapper.readValue(response, ObjectNode.class);
+			} catch (JsonMappingException em) {
+				em.printStackTrace();
+			}catch (JsonProcessingException ep) {
+				ep.printStackTrace();
+			}
+			
+			return objectNode;
+		}
+		
+//		ObjectNode arrayNode = (ObjectNode) objectNode.withArray("nested").get(2);
+//		arrayNode.put("color", provider.get("color"));
 }
